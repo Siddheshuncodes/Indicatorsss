a16db6f7e331e7bfaedbabe2ea8887d7e07cb4f6
Divyesh Mali <medivyeshmali@gmail.com>
2024-09-10 02:18:18 +0530

    added 'home location' component like swiggy as mentioned by mehul

--- screens/CourseListScreen.js
+++ screens/CourseListScreen.js
@@ -1,28 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CourseListScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>HomeScreen</Text>
-    </View>
-  );
-};
+      <SafeAreaView style={styles.container}>
 
-export default CourseListScreen;
+        <Ionicons name="home" size={42} color="black" />
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-        },
-
-        text: {
-            fontSize: 24,
-            fontWeight: "bold",
-            marginBottom: 16,
-        }
-    })
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Home </Text>
+          <Text style={styles.address}>Sneha Nagar, Mhasrul, Nashik</Text>
+        </View>
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      margin: 20, // Adjust this margin as needed
+      paddingLeft: 3,
+      paddingTop: 15
+    },
+    textContainer: {
+      marginLeft: 10, // Space between icon and text
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    address: {
+      fontSize: 14,
+      color: 'gray',
+    },
+  });
+  
+  export default CourseListScreen;
 
 
